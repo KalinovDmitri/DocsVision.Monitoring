8a4404eb7e31220b26420cf80b7ef511d4c8e007
Dmitry Kalinov <kalinov@mercurydevelopment.com>
2018-10-05 13:48:24 +0400

    Addressing task DVM-3 Configure Hangfire - configure Hangfire to use SQL Server database for job persistence; add HangfireDashboardMiddleware to protect dashboard from unauthorized access

--- src/DocsVision.Monitoring/Application/Startup.cs
+++ src/DocsVision.Monitoring/Application/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +17,14 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+using Hangfire;
+using Hangfire.AspNetCore;
+using Hangfire.Dashboard;
+using Hangfire.SqlServer;
+
+using DocsVision.Monitoring.Extensions;
+using DocsVision.Monitoring.Filters;
 
 namespace DocsVision.Monitoring
 {
@@ -32,18 +44,25 @@ namespace DocsVision.Monitoring
 		public IServiceProvider ConfigureServices(IServiceCollection services)
 		{
 			services
+				.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(ConfigureCookieAuthentication);
+
+			services.AddHangfire(ConfigureHangfire);
+			
+			services
 				.AddMvc()
 				.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
 			var provider = services.BuildServiceProvider();
 			return provider;
 		}
-		
+
 		public void Configure(IApplicationBuilder app)
 		{
 			if (_hostingEnvironment.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
 			}
 			else
 			{
@@ -51,8 +70,47 @@ namespace DocsVision.Monitoring
 			}
 
 			app.UseHttpsRedirection();
+
 			app.UseStaticFiles();
+			
+			app.UseAuthentication();
+
 			app.UseMvc(BuildRoutes);
+
+			app.UseHangfireServer();
+			
+			app.UseHangfireDashboardWithAuthentication(options: new DashboardOptions
+			{
+				Authorization = new IDashboardAuthorizationFilter[]
+				{
+					new HangfireAuthorizationFilter()
+				},
+				DisplayStorageConnectionString = false
+			});
+		}
+
+		private void ConfigureCookieAuthentication(CookieAuthenticationOptions options)
+		{
+			options.AccessDeniedPath = new PathString("/Account/Login");
+			options.LoginPath = new PathString("/Account/Login");
+			options.LogoutPath = new PathString("/Account/Logout");
+
+			options.ExpireTimeSpan = TimeSpan.FromDays(1.0D);
+			options.SlidingExpiration = true;
+
+			options.Validate();
+		}
+
+		private void ConfigureHangfire(IGlobalConfiguration configuration)
+		{
+			var connectionString = _configuration.GetConnectionString("System");
+
+			configuration.UseSqlServerStorage(connectionString, new SqlServerStorageOptions
+			{
+				PrepareSchemaIfNecessary = true,
+				SchemaName = "Hangfire",
+				TransactionIsolationLevel = IsolationLevel.ReadCommitted
+			});
 		}
 
 		private void BuildRoutes(IRouteBuilder routeBuilder)
---
+++ src/DocsVision.Monitoring/Filters/HangfireAuthorizationFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace DocsVision.Monitoring.Filters
+{
+	public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+	{
+		public bool Authorize([NotNull] DashboardContext context)
+		{
+			var httpContext = context.GetHttpContext();
+			
+			if (httpContext.User.Identity.IsAuthenticated)
+			{
+				return true;
+			}
+
+			httpContext
+				.ChallengeAsync()
+				.GetAwaiter()
+				.GetResult();
+
+			return false;
+		}
+	}
+}
