8f0423b0e672d21db91cb9db68e78eec1ac8f97a
Dmitry Kalinov <kadmvl@yandex.ru>
2018-10-07 20:19:01 +0400

    Addressing task DVM-2 Configure Entity Framework - added EF Core package to solution projects

---
+++ src/DocsVision.Monitoring.DataModel.Framework/Framework/MonitoringDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DocsVision.Monitoring.DataModel.Framework
+{
+	public class MonitoringDbContext : DbContext
+	{
+		public MonitoringDbContext(DbContextOptions<MonitoringDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+		}
+	}
+}
