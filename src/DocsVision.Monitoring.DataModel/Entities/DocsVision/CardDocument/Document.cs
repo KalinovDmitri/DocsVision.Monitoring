e9fbbfe983e1c575572bfecbcf726bef9b05b7bd
Dmitry Kalinov <kadmvl@yandex.ru>
2018-10-07 18:28:10 +0400

    Addressing task DVM-1 Add data model entities - added common DocsVision entities; added other required data model entities

---
+++ src/DocsVision.Monitoring.DataModel/Entities/DocsVision/CardDocument/Document.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocsVision.Monitoring.DataModel
+{
+	public class Document : BaseCard
+	{
+		public virtual DocumentMainInfo MainInfo { get; set; }
+
+		public virtual DocumentSystemInfo System { get; set; }
+	}
+}
---
+++ src/DocsVision.Monitoring.DataModel/Entities/DocsVision/CardDocument/DocumentMainInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DocsVision.Monitoring.DataModel
+{
+	public class DocumentMainInfo : BaseCardSectionRow
+	{
+		public string Name { get; set; } // nvarchar 480
+
+		public Guid? Author { get; set; }
+
+		public Guid? CategoryList { get; set; }
+
+		public DateTime? RegDate { get; set; }
+
+		public string ExternalNumber { get; set; } // varchar 256
+
+		public string Content { get; set; } // varchar max
+	}
+}
