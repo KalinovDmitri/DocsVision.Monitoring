f55920986075cb07f45187c91301ad18c735b708
Dmitry Kalinov <kalinov@mercurydevelopment.com>
2018-10-10 16:40:53 +0400

    Addressing task DVM-6 Implement monitoring service - added stub for IDocsVisionMonitoringService

---
+++ src/DocsVision.Monitoring.Services/Services/DocsVisionMonitoringService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocsVision.Monitoring.Services
+{
+	public class DocsVisionMonitoringService : IDocsVisionMonitoringService
+	{
+		public async Task ProcessDocumentsWithoutShortcutsAsync()
+		{
+			await Task.Yield();
+		}
+	}
+}
---
+++ src/DocsVision.Monitoring.Services/Services/IDocsVisionMonitoringService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocsVision.Monitoring.Services
+{
+	public interface IDocsVisionMonitoringService
+	{
+		Task ProcessDocumentsWithoutShortcutsAsync();
+	}
+}
