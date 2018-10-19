a6a502afdc232a059f54c77e73b8b83e2ac93aba
Dmitry Kalinov <kadmvl@yandex.ru>
2018-10-19 23:41:40 +0400

    Addressing task DVM-4 Implement layout view - updated navigation bar using Bootstrap 4; reorganize elements on /Home/Index page

--- src/DocsVision.Monitoring/wwwroot/js/site.js
+++ src/DocsVision.Monitoring/wwwroot/js/site.js
@@ -1,3 +1,10 @@
 ﻿$(document).ready(function () {
-    $("#menu").kendoMenu();
+    $(body).on("mouseenter mouseleave", ".dropdown", function (e) {
+        let dd = $(e.target).closest("dropdown");
+        dd.addClass("show");
+        setTimeout(function () {
+            dd[dd.is(":hover") ? "addClass" : "removeClass"]("show");
+            $("[data-toggle='dropdown']", dd).attr("aria-expanded", dd.is(":hover"));
+        }, 300);
+    });
 });
