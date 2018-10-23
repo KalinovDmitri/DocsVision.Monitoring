29da20f7542cbb8a03b7dc141445efc91f169d02
Dmitry Kalinov <kalinov@mercurydevelopment.com>
2018-10-23 15:18:31 +0400

    Addressing task DVM-7 Setting up as Windows service - configured application to host and start as Windows service

--- src/DocsVision.Monitoring/Application/Program.cs
+++ src/DocsVision.Monitoring/Application/Program.cs
@@ -1,27 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Logging;
 
 namespace DocsVision.Monitoring
 {
 	public class Program
 	{
+		private const string ConsoleArgument = "--console";
+
 		public static void Main(string[] args)
 		{
-			IWebHostBuilder builder = CreateWebHostBuilder(args);
+			var isService = !(Debugger.IsAttached || (-1 != Array.IndexOf(args, ConsoleArgument)));
+			var builder = CreateWebHostBuilder(GetWebHostArgs(args));
+
+			if (isService)
+			{
+				var pathToExe = Assembly.GetExecutingAssembly().Location;
+				var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+				builder.UseContentRoot(pathToContentRoot);
+			}
+			
 			IWebHost host = builder.Build();
-			host.Run();
+
+			if (isService)
+			{
+				host.RunAsService();
+			}
+			else
+			{
+				host.Run();
+			}
+		}
+
+		private static string[] GetWebHostArgs(string[] args)
+		{
+			return args
+				.Where(x => x != ConsoleArgument)
+				.ToArray();
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost
 				.CreateDefaultBuilder(args)
+				.UseKestrel()
 				.UseStartup<Startup>();
 	}
 }
