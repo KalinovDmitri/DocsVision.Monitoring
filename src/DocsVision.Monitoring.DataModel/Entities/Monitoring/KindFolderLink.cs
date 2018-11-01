3451a08e48aee1c3df0b225518bbee7eab99de21
Dmitry Kalinov <kadmvl@yandex.ru>
2018-11-01 09:07:21 +0400

    Addressing task DVM-1 Add data model entities - added entities for reporting; updated KindFolderLink

--- src/DocsVision.Monitoring.DataModel/Entities/Monitoring/KindFolderLink.cs
+++ src/DocsVision.Monitoring.DataModel/Entities/Monitoring/KindFolderLink.cs
@@ -6,9 +6,13 @@ namespace DocsVision.Monitoring.DataModel
 	{
 		public Guid KindID { get; set; }
 
+		public string KindName { get; set; }
+
 		public string KindFullName { get; set; }
 
 		public Guid FolderID { get; set; }
+
+		public string FolderName { get; set; }
 
 		public string FolderFullName { get; set; }
 
---
+++ src/DocsVision.Monitoring.DataModel/Entities/Monitoring/Reports/DocumentsWithoutShortcutsReportRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DocsVision.Monitoring.DataModel
+{
+	public class DocumentsWithoutShortcutsReportRow : BaseEntity<long>
+	{
+		public long ReportId { get; set; }
+
+		public long? LinkId { get; set; }
+		
+		public Guid DocumentId { get; set; }
+
+		public string DocumentName { get; set; }
+
+		public string DocumentDescription { get; set; }
+
+		public string DocumentKindName { get; set; }
+
+		public string TargetFolderName { get; set; }
+	}
+}
